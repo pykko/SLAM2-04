b97e422c08c43489dc696232547db85622ff4d30
pykko <pykko@xilim.fr>
2019-01-23 13:17:35 +0100

    Sources : AppXDQ.java

---
+++ Exo-3-EtatsParties/AppXDQ.java
@@ -0,0 +1,89 @@
+package fr.gns.xdq;
+
+public class AppXDQ {
+
+	public static void main(String[] args) {
+		
+		// Tous les affichages doivent faire apparaître le numéro de la question
+		
+		// ATTENTION, la date de fin d'une partie doit être renseignée lors d'une
+		// annulation, d'un abandon ou d'une victoire
+		
+		// 1- Créer puis afficher la partie référencée par la variable partie1
+		//		Caractéristiques de la partie :
+		//			Numéro : 1
+		//			État : En attennte d'un adversaire
+		
+		/* Votre code pour la question 1 ici */
+
+
+
+
+
+		// 2- Tenter de rejoindre la partie
+		//		Si l'opération s'est déroulée avec succés, afficher "2- Partie en cours"
+		//		Sinon afficher "2- La partie n'a pu être rejointe"
+		// 3- ... puis afficher la partie
+		
+		/* Votre code pour les questions 2 et 3 ici */
+		
+		
+		
+		
+		
+		
+		// 4- Afficher l'état de la partie
+		//		Si la partie est en attente d'un adversaire, afficher "4- Partie en attente d'un adversaire"
+		//		Si la partie est en cours "4- Partie en cours."
+		//		Si la partie est annulée "4- Partie annulée."
+		//		Si ...
+		
+		/* Votre code pour la question 4 ici */
+		
+		
+		
+		
+		
+		// 5- Gagner puis afficher la partie
+		
+		/* Votre code pour la question 5 ici */
+		
+		
+		
+		
+		
+		// 6- Afficher l'état de la partie 
+		//		(voir question 4)
+		//		...
+
+		
+		/* Votre code pour la question 6 ici */
+		
+		
+		
+						
+		
+		// 7- Tenter d'annuler la partie
+		//		Si l'opération d'annulation est réalisée avec succès, afficher "7- Partie annulée."
+		//		Sinon afficher "7- Annulation refusée."
+		// 8- ... puis afficher la partie
+		
+		/* Votre code pour les questions 7 et 8 ici */
+		
+		
+		
+		
+		
+		// 9- Tenter d'abandonner la partie
+		//		Si l'opération d'abandon est réalisée avec succès, afficher "9- Partie abandonnée."
+		//		Sinon afficher "9- Abandon refusé."
+		// 10- ... puis afficher la partie
+		
+		/* Votre code pour les questions 9 et 10 ici */
+		
+					
+	
+
+	}
+
+}
