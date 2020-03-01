d8b94538b3a5763f2018ce9737afd3e185175e09
pyko <pyko@xilim.com>
2020-03-01 18:09:18 +0100

    Participation

---
+++ AppAdmin.java
@@ -0,0 +1,96 @@
+package fr.awa.awajaba;
+
+public class AppAdmin {
+
+	public static void main(String[] args) {
+		
+		// Tous les affichages doivent faire apparaître le numéro de la question
+		
+		
+		// 1- Créer puis afficher la participation référencée par la variable pr1
+		//		Caractéristiques de la participation :
+		//			Numéro : 1
+		//			État : Réservée
+		
+		/* Votre code pour la question 1 ici */
+
+
+
+
+
+		// 2- Tenter de confirmer la participation 1
+		//		Si l'opération s'est déroulée avec succés, afficher "2- Participation confirmée"
+		//		Sinon afficher "2- La participation n'a pu être confirmée"
+		// 3- ... puis afficher la participation
+		
+		/* Votre code pour les questions 2 et 3 ici */
+		
+		
+		
+		
+		
+		
+		// 4- Afficher l'état de la participation
+		//		Si la partie est réservée afficher "4- Participation réservée."
+		//		Si la partie est confirmée "4- Participation confirmée."
+		//		Si la partie est annulée "4- Participation annulée."
+		//		Si ...
+		
+		/* Votre code pour la question 4 ici */
+		
+		
+		
+		
+		
+		// 5- Honorer puis afficher la participation
+		
+		/* Votre code pour la question 5 ici */
+		
+		
+		
+		
+		
+		// 6- Afficher l'état de la participation 
+		//		(voir question 4)
+		//		...
+
+		
+		/* Votre code pour la question 6 ici */
+		
+		
+		
+						
+		
+		// 7- Tenter d'annuler la participation
+		//		Si l'opération d'annulation est réalisée avec succès, afficher "7- Participation annulée."
+		//		Sinon afficher "7- Annulation refusée."
+		// 8- ... puis afficher la participation
+		
+		/* Votre code pour les questions 7 et 8 ici */
+		
+		
+		
+		
+		
+		// 9- Tenter de non honorer la participation
+		//		Si l'opération est réalisée avec succès, afficher "9- Participation non honorée."
+		//		Sinon afficher "9- Opération refusée."
+		// 10- ... puis afficher la participation
+		
+		/* Votre code pour les questions 9 et 10 ici */
+		
+					
+	
+	
+	
+		// 11- Tenter d'honorer la participation
+		//		Si l'opération est réalisée avec succès, afficher "11- Participation honorée."
+		//		Sinon afficher "12- Opération refusée."
+		// 12- ... puis afficher la participation
+		
+		/* Votre code pour les questions 11 et 12 ici */
+	
+
+	}
+
+}
